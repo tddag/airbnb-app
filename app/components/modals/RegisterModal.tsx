afa106fa9760f3fcf2d18739d5eed8706060b74d
Tam Dang <danhtam0509@gmail.com>
2023-05-14 23:10:05 -0400

    finished listing creation step 1 category selection

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -18,8 +18,11 @@ import Input from '../inputs/Input'
 import { toast } from 'react-hot-toast'
 import Button from '../Button'
 import { signIn } from 'next-auth/react'
+import useLoginModal from '@/app/hooks/useLoginModal'
 
 const RegisterModal = () => {
+
+  const loginModal = useLoginModal()
   const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false)
 
@@ -51,6 +54,12 @@ const RegisterModal = () => {
         setIsLoading(false)
       })
   }
+
+  const toggle = useCallback(() => {
+    registerModal.onClose()
+    loginModal.onOpen();    
+  }, [loginModal, registerModal])
+
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -107,7 +116,7 @@ const RegisterModal = () => {
             Already have an account?
           </div>
           <div 
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className="text-neutral-800 cursor-pointer hover:underline">
             Log in 
           </div>          
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal'
 
 import { signOut } from 'next-auth/react'
 import { SafeUser } from '@/app/types'
+import useRentModal from '@/app/hooks/useRentModal'
 
 interface UserMenuProps {
     currentUser?: SafeUser | null
@@ -20,6 +21,8 @@ const UserMenu: React.FC<UserMenuProps> = ({
 
     const registerModal = useRegisterModal()
     const loginModal = useLoginModal()
+    const rentModal = useRentModal()
+
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(
@@ -28,12 +31,18 @@ const UserMenu: React.FC<UserMenuProps> = ({
       },
       [],
     )
+
+    const onRent = useCallback(() => {
+        if (!currentUser) return loginModal.onOpen();
+
+        rentModal.onOpen()
+    }, [currentUser, loginModal, rentModal])
     
 
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
-            <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+            <div onClick={onRent} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                 Airbnb your home
             </div>
 
@@ -41,7 +50,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 <AiOutlineMenu/>
                 <div className="hidden md:block">
                     <Avatar src={currentUser?.image}/>
-
+ 
                 </div>
             </div>
 
@@ -65,7 +74,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                             <MenuItem onClick={() => {}} label="My favorites"/>
                             <MenuItem onClick={() => {}} label="My reservations"/>
                             <MenuItem onClick={() => {}} label="My properties"/>
-                            <MenuItem onClick={() => {}} label="Airbnb my home"/>
+                            <MenuItem onClick={rentModal.onOpen} label="Airbnb my home"/>
                             <hr/>
                             <MenuItem onClick={() => {signOut()}} label="Logout"/>
                         </>
