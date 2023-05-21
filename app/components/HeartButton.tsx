bc65e3af35db70f9f93eaf4e2a8208d044bfc3b9
tddag <danhtam0509@gmail.com>
2023-05-21 13:46:15 -0400

    finished favoriting functionality, api favorites route post and delete, useFavorite hook to check hasFavorited and toggleFavorite

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeUser } from '../types';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '../hooks/useFavorite';
 
 interface HeartButtonProps {
     listingId: string;
@@ -11,8 +12,12 @@ const HeartButton: React.FC<HeartButtonProps>= ({
     listingId,
     currentUser
 }) => {
-    const hasFavorited = true;
-    const toggleFavorite = () => {}
+    
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+    })
+
   return (
     <div 
         onClick={toggleFavorite}
